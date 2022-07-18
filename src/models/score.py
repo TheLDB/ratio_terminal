8cef603e79a93b90a895b80ce221e676b89cc7c8
kitwnd <hello@kitwnd.dev>
2022-07-18 10:14:07 -0600

    🔨 Fix ZeroDivisionError

--- src/models/score.py
+++ src/models/score.py
@@ -6,11 +6,16 @@ class Score:
     accepted: int
     declined: int
     final: int = field(init=False)
-    accepted_ratio: float = field(init=False)
-    declined_ratio: float = field(init=False)
+    accepted_ratio: float = field(init=False, default=0.0)
+    declined_ratio: float = field(init=False, default=0.0)
 
     def __post_init__(self):
         self.final = self.accepted - self.declined
         total = self.accepted + self.declined
+
+        if self.declined is 0:
+            # Prevent a ZeroDivisionError
+            return
+        
         self.accepted_ratio = round((self.accepted / total) * 100, 1)
         self.declined_ratio = round(100 - self.accepted_ratio, 1)
